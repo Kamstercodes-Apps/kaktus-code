f0d00ba8ae424b80aeb8e8bc7ad3d1f6bb2cb7bd
Jojo <jojo10stern@gmail.com>
2025-08-08 16:04:36 +0200

    Initial upload

---
+++ public/script.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDAf54xuNXu1wW4oMoSqSdLi8dxW6LtEnI",
+    authDomain: "kaktus-code.firebaseapp.com",
+    projectId: "kaktus-code",
+    storageBucket: "kaktus-code.firebasestorage.app",
+    messagingSenderId: "701934045225",
+    appId: "1:701934045225:web:d2509275e6ef2977fd7438"
+};
+
+const app = initializeApp(firebaseConfig);
